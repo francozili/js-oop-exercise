c9e5e9793245fc77043761db2b2267129952485b
francis_chukwuma <francischukwuma@PSADMINs-MacBook-Air.local>
2019-05-06 09:33:36 -0400

    first commit

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,9 +1,17 @@
 console.log('tamagotchi file is loaded');
 
-//your class declaration here
-
-
-//create new Tamagotchis
-
-
-//test out your Tamagotchies below via console.logs
+class Tamagotchi {
+    constructor(name, creatureType){
+      this.name = name;
+      this.creatureType = creatureType;
+      this.foodInTummy = 10;
+      this.restedness = 10;
+      this.health = 10;
+    }
+  
+    cry() {
+      this.foodInTummy--;
+      console.log("WAAAH!!!!!!");
+      console.log(this.name + ' has current food in tummy = ' + this.foodInTummy);
+    };
+   
